8e501e2b37b070ceb5a10abf78eead523420315d
Lordchief <lordchief21@gmail.com>
2023-11-10 18:26:13 -0800

    Configure zlib deflate

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ const app = express();
 const server = http.createServer(app)
 
 // Declare and initialize the WebSocket instance
-const wss = new WebSocket.Server({server,concurrencyLimit: 10});
+const wss = new WebSocket.Server({server, perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+    }
+});
 
 wss.on("connection",(ws)=>{
     
@@ -26,7 +45,7 @@ wss.on("connection",(ws)=>{
 
 
 server.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`)
+    console.log(` WebSocket Server is Opened`)
 })
 
 
