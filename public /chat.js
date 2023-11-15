eae342ac79e109a77f437e3fe0f5058473e413ac
Lordchief <lordchief21@gmail.com>
2023-11-15 10:15:04 -0800

    Migrate JS code

--- public /chat.js
+++ public /chat.js
@@ -1,47 +1,58 @@
-function start()  {
+(function ()  {
 
-        let sendButton = document.querySelector("#sendmsj");
-        let messages = document.querySelector("#message");
-        const messageBox = document.querySelector("#messageContainer");
+    let sendButton = document.querySelector("#sendmsj");
+    let messages = document.querySelector("#chat-frame");
+    const messageBox = document.querySelector("#messageContainer");
+    
 
 
-        let ws;
+    let ws;
 
-        const showMessage = (msj) => {
-            messages.textContent += ` \n\n ${msj}`;
-            messages.scrollTop = messages.scrollHeight;
-            messages.value = "";
+    const showMessage = (msj, nameClass) => {
+        const para = document.createElement("p")
+        const node = document.createTextNode(`\n${msj}`)
+        para.appendChild(node)
+        document.getElementById("chat-frame")
+        document.querySelector("#chat-frame").appendChild(para)
+        para.className = nameClass
+        para.style.borderRadius = "15px"
+        // para.textContent += `\n${msj}`;
+        messages.scrollTop = messages.scrollHeight;
+        messages.value = "";
+    }
+
+
+    function init(){
+        if(ws) {
+            ws.onerror = ws.onopen = ws.onclose = null;
+            ws.close();
         }
 
-        function init(){
-            if(!ws) {
-                ws.close();
-            }
-    
-            ws = new WebSocket('ws://localhost:9000');
-            ws.onopen = () => {
-                console.log('WebSocket Connected')
-            }
-    
-            ws.onmessage = ({data}) => {
-                console.log(data)
-                showMessage(data)
-            };
+        ws = new WebSocket('ws://localhost:9000');
+        ws.onopen = () => {
+            console.log('WebSocket Connected')
+        }
 
-            ws.onclose = () => {
-                ws = null
-            };
-
-            sendButton.onclick = () => {
-                if(!ws) {
-                    showMessage('An error ocurred with WebSocket')
-                }
-                
-                ws.send(messageBox);
-            }
+        ws.onmessage = ({data}) => {
+            console.log(data)
+            showMessage(data, "alert alert-warning bubble-chat_received")
+            
         };
 
-        init()
-}
+        ws.onclose = () => {
+            ws = null
+        };
 
-start();
+        sendButton.onclick = () => {
+            if(!ws) {
+                showMessage('An error ocurred with WebSocket')
+                return ;
+            }
+            
+            ws.send(messageBox.value);
+            showMessage(messageBox.value, "alert alert-primary bubble-chat");
+        }
+    };
+
+    init()
+} )()
