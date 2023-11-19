db336a51a5c2842cad7331c6f1acf222464bb379
Lordchief <lordchief21@gmail.com>
2023-11-19 07:32:58 +0000

    "Identify chat users"

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 const http = require('http')
 const express = require('express')
 const {WebSocket} = require('ws');
-const  bodyParser = require('body-parser')
+const  bodyParser = require('body-parser');
+const { constants } = require('fs/promises');
 
 
 const port = 9000;
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+
 let userdata = "";
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
-
-
-app.get('/t', (req,res) => {
-    res.sendFile(__dirname+"/public/userRegister.html")
-    userdata = req.body
-   
-    
-})
-
-app.post('/', async (req,res) => {
-    userdata = req.body
-    console.log(userdata)
-    res.sendFile(__dirname+"/public/index.html");
-})
-
-
 
 // Declare and initialize the WebSocket instance
 const wss = new WebSocket.Server({server, perMessageDeflate: {
@@ -52,21 +33,70 @@ const wss = new WebSocket.Server({server, perMessageDeflate: {
     }
 });
 
+//Declare new function to generate unique ID per user
+
+wss.getUniqueId = () => {
+    const genId = () => {
+        return Math.floor((1+Math.random()) * 0x10000).toString(8).substring(1)
+    }
+    return genId() + genId() - genId()
+}
 
 
-wss.on("connection",(ws)=>{
-    
+//Open  WebSocket connection and send or received message from/to client.
+
+wss.on('connection', (ws,req)=>{
+    ws.id = req.headers['sec-websocket-key']
     ws.on("message", (msj, isBinary) =>{
         wss.clients.forEach((client) => {
-           
             if ( client != ws && client.readyState == WebSocket.OPEN) {
                 client.send(msj, {binary: isBinary});
+                    console.log(
+                        {   id: client.id,
+                            username: userdata,
+                            content:msj,                        
+                        })
             }
         })
-        
+            
     })
+   
+
     
 })
+
+
+
+
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname+"/public/index.html")  
+})
+
+app.post('/', async (req,res) => {
+    userdata =  req.body
+    console.log(userdata)
+    res.redirect("/chat")
+    
+    
+})
+
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname+"/public/chat.html")
+})
+
+
+
+   
+
 
 
 
